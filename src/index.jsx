2145732b520955fb2a5ab8de148d29726777e353
Levy Moreira <lmoreira@guidewire.com>
2017-05-27 23:56:55 +0100

    Add eslint

--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,10 @@ render( <AppContainer><App/></AppContainer>, document.querySelector("#app"));
 
 if (module && module.hot) {
   module.hot.accept('./app.jsx', () => {
-    const App = require('./app.jsx').default;
+    const AppHot = require('./app.jsx').default;
     render(
       <AppContainer>
-        <App/>
+        <AppHot/>
       </AppContainer>,
       document.querySelector("#app")
     );
